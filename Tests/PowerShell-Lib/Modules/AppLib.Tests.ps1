fc3d0d01add4413cfca06dc5aadab0f7e2cc0647
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-08-29 22:08:58 +0200

    2 Tests
    + Test for "Install-App"
    + Test for "Test-AppInstalled"

--- Tests/PowerShell-Lib/Modules/AppLib.Tests.ps1
+++ Tests/PowerShell-Lib/Modules/AppLib.Tests.ps1
@@ -36,3 +36,68 @@ Describe "Get-AppsInstalled" {
         }
     }
 }
+
+Describe "Install-App" {
+    $InstallerPathExe = "TestDrive:\x.exe"
+    $InstallerPathMsi = "TestDrive:\x.msi"
+
+    New-Item -Path $InstallerPathExe -ItemType "File"
+    New-Item -Path $InstallerPathMsi -ItemType "File"
+
+    Context "exe" {
+        Mock Start-Process {} -ModuleName "AppLib"
+
+        It "should give no feedback" {
+            Install-App -InstallerPath $InstallerPathExe | Should BeNullOrEmpty
+        }
+    }
+
+    Context "msi" {
+        Mock Start-Process {} -ModuleName "AppLib"
+
+        It "should give no feedback" {
+            Install-App -InstallerPath $InstallerPathMsi | Should BeNullOrEmpty
+        }
+    }
+
+    Context "default" {
+        $InstallerPath = "~"
+
+        It "should throw an error" {
+            {Install-App -InstallerPath $InstallerPath} | Should Throw "File extension of `"$InstallerPath`" is neither `".exe`" nor `".msi`"."
+        }
+    }
+}
+
+Describe "Test-AppInstalled" {
+    $AppName = "TestApp-123"
+    $AppNameRegex = "^TestApp-123$"
+
+    Context "app installed" {
+        Mock Get-AppsInstalled {
+            Return @(
+                @{
+                    "DisplayName" = "TestApp-123"
+                }
+            )
+        } -ModuleName "AppLib"
+
+        It "should return true for an installed app" {
+            Test-AppInstalled -AppName $AppName | Should Be $True
+            Test-AppInstalled -AppName $AppNameRegex -RegexCompare | Should Be $True
+        }
+    }
+
+    Context "app not installed" {
+        Mock Get-AppsInstalled {
+            Return @(
+                @{}
+            )
+        } -ModuleName "AppLib"
+
+        It "should return false for a not installed app" {
+            Test-AppInstalled -AppName $AppName | Should Be $False
+            Test-AppInstalled -AppName $AppNameRegex -RegexCompare | Should Be $False
+        }
+    }
+}
