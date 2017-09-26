0fe520867258fd4ce8faabc87997e0ac375ca67d
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-09-26 21:08:51 +0200

    ProjectLib Tests
    + 2 ProjectLib Tests

---
+++ Tests/PowerShell-Lib/Modules/ProjectLib.Tests.ps1
@@ -0,0 +1,56 @@
+Set-StrictMode -Version Latest
+
+Import-Module -Name "${PSScriptRoot}\..\..\..\PowerShell-Lib\PowerShell-Lib.psd1" -Force
+Import-Module -Name "${PSScriptRoot}\..\..\..\PowerShell-Lib\Modules\ProjectLib.psm1" -Force
+
+Describe "New-ModuleMarkdown" {
+    New-Item -Path "TestDrive:\Modules\" -Name "ModuleALib.ps1" -ItemType "File" -Value @"
+    Function Get-Module {}
+"@ -Force
+
+    New-Item -Path "TestDrive:\Modules\" -Name "ModuleBLib.ps1" -ItemType "File" -Value @"
+    Function Install-Module {}
+"@ -Force
+
+    It "creates a markdown list for all functions" {
+        New-ModuleMarkdown -SourcePath @("TestDrive:\Modules\") -DocPath "Docs" | Should Be @"
+## Modules
+- **ModuleBLib**
+  - [Install-Module](Docs/Install-Module.md)
+- **ModuleALib**
+  - [Get-Module](Docs/Get-Module.md)
+"@
+    }
+
+    It "creates a sorted markdown list for all functions" {
+        New-ModuleMarkdown -SourcePath @("TestDrive:\Modules\") -DocPath "Docs" -Sort | Should Be @"
+## Modules
+- **ModuleALib**
+  - [Get-Module](Docs/Get-Module.md)
+- **ModuleBLib**
+  - [Install-Module](Docs/Install-Module.md)
+"@
+    }
+}
+
+Describe "Read-Settings" {
+    New-Item -Path "TestDrive:\" -Name "settings-a.json" -ItemType "File" -Value @"
+{
+    "Key": "value-a"
+}
+"@
+
+    New-Item -Path "TestDrive:\" -Name "settings-b.json" -ItemType "File" -Value @"
+{
+    "Key": "value-b"
+}
+"@
+
+    It "reads settings from JSON to PSCustomObject" {
+        ConvertTo-Json (Read-Settings -SourcePath @("TestDrive:\settings-a.json", "TestDrive:\settings-b.json")) | Should Be @"
+{
+    "Key":  "value-b"
+}
+"@
+    }
+}
