660ff8522610c0ae324a3d93a84afc211ab4db1a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2017-10-10 23:31:23 +0200

    Invoke Running Test
    + Test-DockerRunning on Invoke-Docker

--- PowerShell-Lib/Modules/DockerLib.psm1
+++ PowerShell-Lib/Modules/DockerLib.psm1
@@ -171,6 +171,10 @@ Function Invoke-Docker {
         Throw "Command `"docker`" not found."
     }
 
+    If (-Not (Test-DockerRunning)) {
+        Throw "Docker is not running."
+    }
+
     docker $Args
 }
 
